ca8b2df9e8a1acca44deef8725b88daf1de29cf9
ambewas <ambewas@users.noreply.github.com>
2018-08-21 10:03:27 +0200

    Update index.js
    add callback

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 import { curry } from "ramda";
 
-const toggleState = curry((someClass, stateProp, e) => {
-	// note - the event is usually not used, but adding the extra param allows this to be used directly instead of in an arrow:
-	// onClick={toggleState("settingsModal", this)}
+const toggleState = curry((someClass, stateProp, callback) => { // eslint-disable-line no-unused-vars
+	// note - if you do not provide the callback, it's possible to use it directly in an onClick prop, as it will return a new function:
+	// onClick={toggleState(this, "settingsModal")}
+	// or do this in the constructor:
+	// this.toggleState = toggleState(this)
 	if (someClass.props && someClass.props.onStateToggle) {
 		someClass.props.onStateToggle(stateProp, !someClass.state[stateProp]);
 	}
-	someClass.setState(prevState => ({ [stateProp]: !prevState[stateProp] }));
+	someClass.setState(prevState => ({ [stateProp]: !prevState[stateProp] }), typeof callback === "function" ? callback : undefined);
 });
-
 
 export default toggleState;
